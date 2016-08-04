2af4a7bece6ad077341c9eba15a5da7b01c6f972
Eli Murray <emurray@tampabay.com>
2016-08-04 12:49:29 -0400

    fixed image resizer

--- src/cli/logger.ts
+++ src/cli/logger.ts
@@ -1,9 +1,12 @@
-import { createLogger } from "bunyan";
+import { createLogger, stdSerializers } from "bunyan";
 import { resolve } from 'path';
 
 export function makeLogger(ledeHome, level = "info") {
   return createLogger({
     name: "LedeLogger",
+    serializers: {
+      err: stdSerializers.err
+    },
     streams: [
       {
         level: level,
