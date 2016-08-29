431b25c5c4d7e5db97a8aff7f83c0fdf339ab160
Eli Murray <emurray@tampabay.com>
2016-08-28 22:40:10 -0400

    built new NunjucksCompiler constructor

---
+++ src/rebuild/ComponentExtension.ts
@@ -0,0 +1,25 @@
+import { Extension } from 'nunjucks';
+
+
+export default class ComponentExtension implements Extension {
+  tags: string[];
+  elementName: string;
+
+  constructor({tags, elementName}) {
+    this.tags = tags;
+    this.elementName = elementName;
+  };
+
+  parse(parser, nodes, lexer) {
+    const token = parser.nextToken();
+    const args = parser.parseSignature(null, true);
+    parser.advanceAfterBlockEnd(token.value);
+    this.elementName = args.children[0].value;
+    return new nodes.CallExtensionAsync(this, 'run', args);
+  }
+
+  run(context, arg, cb) {
+    let ctx = Object.assign({}, context.ctx, { [this.elementName]: arg});
+    context.env.renderString(arg.template, ctx, cb);
+  }
+}
