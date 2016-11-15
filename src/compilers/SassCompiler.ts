432c87d772a50d89f48b9c8b47b7af83594479fb
Eli Murray <emurray@tampabay.com>
2016-11-15 14:06:00 -0500

    removed legacy sasscompiler

--- src/compilers/SassCompiler.ts
+++ src/compilers/SassCompiler.ts
@@ -8,117 +8,7 @@ import { mockLogger } from "../utils";
 import { SassFailed } from "../errors/CompilerErrors";
 
 
-export class SassCompiler implements MaterialCompiler {
-  logger: Logger;
-  renderOpts: Options;
-  cacheDir: string;
-
-  constructor(arg?) {
-    this.logger = <Logger><any>mockLogger;
-    this.renderOpts = Object.assign({}, {outputStyle: "compressed", sourceComments: false}, arg && arg.opts ? arg.opts : {});
-    this.cacheDir = arg && arg.cacheDir ? arg.cacheDir : ".ledeCache";
-  };
-
-  static renderFile(filePath, opts) {
-    opts.file = filePath;
-    return new Promise((resolve, reject) => {
-      render(opts, (err, res) => {
-        if (err) return reject(err);
-        return resolve(res);
-      });
-    });
-  }
-
-  configure({ logger }) {
-    this.logger = logger;
-  }
-
-  async compile(tree: ProjectModel) {
-    const cachePath = join(tree.workingDir, this.cacheDir);
-    let globals, bits;
-
-    try {
-      await this.buildCache(cachePath, tree);
-    } catch (err) {
-      // this.logger.error({err}, "An error occurred while caching styles");
-      throw new SassFailed({detail: err});
-    }
-    try {
-      globals = await this.compileGlobals(cachePath, tree);
-    } catch (err) {
-      // this.logger.error({err}, "An error occurred while compiling global styles");
-      throw new SassFailed({detail: err});
-    }
-    try {
-      bits = await this.compileBits(cachePath, tree);
-    } catch (err) {
-      // this.logger.error({err}, "An error occurred while compiling bit styles");
-      throw new SassFailed({detail: err});
-    }
-
-    return { bits, globals };
-  };
-
-  async compileBits(cachePath: string, tree: ProjectModel) {
-    const bits = {};
-    return Promise.all(tree.pages.map(page => {
-      const pageCachePath = join(cachePath, page.context.$PAGE.$name);
-      const includePaths = [join(pageCachePath, "styles")];
-      return Promise.all(page.styles.bits.map(mat => {
-        return SassCompiler.renderFile(mat, Object.assign({}, this.renderOpts, {includePaths}));
-      })).then(renderedBits => {
-        bits[page.context.$PAGE.$name] = renderedBits.reduce((state, rendered: { css: Buffer }) => {
-          return state + rendered.css.toString();
-        }, "");
-      });
-    })).then(() => {
-      return bits;
-    });
-  }
-
-  async compileGlobals(cachePath: string, tree: ProjectModel) {
-    const globals = {};
-    return Promise.all(tree.pages.map(page => {
-      const pageCachePath = join(cachePath, page.context.$PAGE.$name);
-      const includePaths = [join(pageCachePath, "styles")];
-      return Promise.all(page.styles.globals.map(mat => {
-        return SassCompiler.renderFile(mat.path, Object.assign({}, this.renderOpts, {includePaths}));
-      })).then(renderedGlobals => {
-        globals[page.context.$PAGE.$name] = renderedGlobals.reduce((state, rendered: { css: Buffer }) => {
-          return state + rendered.css.toString();
-        }, "");
-      });
-    })).then(() => {
-      return globals;
-    });
-  }
-
-  async buildCache(cachePath: string, tree: ProjectModel) {
-    // this.logger.debug({tree}, "Project tree");
-    tree.pages.forEach((page: PageModel) => {
-      const bitPathRegex = new RegExp(".*\/(.*\/.*\.scss)$");
-      const pageCachePath = join(cachePath, page.context.$PAGE.$name);
-
-      return Promise.all([
-        Promise.all(page.cache.styles.map(mat => {
-          return sander.copyFile(mat.path)
-            .to(join(pageCachePath, "styles", mat.overridableName || mat.name || basename(mat.path)));
-        })).then(() => {
-          return Promise.all(page.styles.globals.map(mat => {
-            return sander.copyFile(mat.path)
-              .to(join(pageCachePath, "styles", mat.overridableName || mat.name || basename(mat.path)));
-          }));
-        }),
-        Promise.all(page.styles.bits.map(mat => {
-          return sander.copyFile(mat)
-            .to(join(pageCachePath, "bits", mat.match(bitPathRegex)[1]));
-        }))
-      ]);
-    });
-  }
-}
-
-export class SC {
+export class SassCompiler {
   logger: Logger;
   renderOpts: Options;
   cacheDir: string;
@@ -152,7 +42,7 @@ export class SC {
     const includePaths = [join(pageCachePath, "styles")];
 
     return Promise.all(tree.styles.bits.map(mat => {
-      return this.renderFile(mat, Object.assign({}, this.renderOpts, {includePaths}))
+      return this.renderFile(mat, Object.assign({}, this.renderOpts, {includePaths}));
     })).then(x => x.css.toString("utf8"));
   }
 
