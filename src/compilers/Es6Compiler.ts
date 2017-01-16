33b90fa5e6989d7247ca22d6f15c9f0a35225a00
Eli Murray <emurray@tampabay.com>
2017-01-16 12:50:44 -0500

    2.0.42

--- src/compilers/Es6Compiler.ts
+++ src/compilers/Es6Compiler.ts
@@ -1,7 +1,7 @@
 import { Logger } from "bunyan";
 import { join, basename, dirname } from "path";
 const sander = require("sander");
-import includes from "rollup-plugin-includepaths";
+// import includes from "rollup-plugin-includepaths";
 
 import { MaterialCompiler, PageTree } from "../interfaces";
 import { mockLogger } from "../utils";
